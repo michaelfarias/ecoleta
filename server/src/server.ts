39ee50359667d95b0cb66dcd4c1beab075615402
michaelfarias <michaelfarias01@gmail.com>
2020-06-02 18:34:34 -0300

    implementation of features on the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,35 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários')
+app.use(cors());
 
-    response.json([
-        'Antônio',
-        'Michael',
-        'Farias',
-        'Soares'
-    ]);
-});
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
+
+// Rotas: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando no sistema
+
+// GET: Buscar um ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST: http://localhost:3333/users = Criar um usuário
+// GET: http://localhost:3333/users = Listar usuários
+// GET: http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
+
+// Request Param: Parâmetros que vem na própria rota que identifica um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualização de informações
+
+// SELECT * FROM users WHERE name = 'Michael'
+// knex('users').where('name', 'Michael').select('*')
